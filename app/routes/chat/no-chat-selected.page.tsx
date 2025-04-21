7e1fb45f5e6548227789ae6ce22bbfc2e019faa3
s-javier <javiersalamank@gmail.com>
2025-04-21 14:45:15 -0400

    Wip

--- app/routes/chat/no-chat-selected.page.tsx
+++ app/routes/chat/no-chat-selected.page.tsx
@@ -1,4 +1,14 @@
-export default function NoChatSelectedPage() {
+import type { Route } from './+types/no-chat-selected.page'
+
+export async function loader() {
+  return {
+    msg: 'Hola',
+  }
+}
+
+export default function NoChatSelectedPage({ loaderData }: Route.ComponentProps) {
+  console.log(loaderData)
+
   return (
     <>
       <h1 className="text-2xl/8 font-semibold text-zinc-950 sm:text-xl/8 dark:text-white">
